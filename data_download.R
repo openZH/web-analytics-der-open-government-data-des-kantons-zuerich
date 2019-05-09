bb72f47b4bcb7d5f8bf88a081d6d17d06f87435b
Thomas Knecht <thomas.knecht@statistik.ji.zh.ch>
2019-05-09 18:31:37 +0200

    add more functions to retrieve the groups out of the opendataswiss-metadata

--- data_download.R
+++ data_download.R
@@ -1,33 +1,126 @@
 library(ckanr)
 library(tidyverse)
-library(jsonlite)
 
 
-ckanr_setup(url = "https://opendata.swiss/")
+
+# matomo token needed to query the API
+token <- "your token"
 
 
-test <- package_search(fq = 'organization:statistisches-amt-kanton-zuerich', rows= 1000, as='table')
-
-test_1 <- test$results
+month <- as.Date(paste0(format(as.Date(Sys.Date()), "%Y-%m"),"-01"))-1
 
 
-test_1_selected <- test_1 %>% select(id, groups)
-
-test_1_list <- split(test_1_selected, seq(nrow(test_1_selected)))
-
-extract_language <- function(x){
+getOrganizationsZH <- function(token){
+  
+  data_organization <- read.csv(paste0("https://piwik.opendata.swiss/index.php?date=2019-04-30&expanded=1&filter_limit=-1&
+                                     format=CSV&idDimension=1&idSite=1&language=en&method=CustomDimensions.getCustomDimension&
+                                       module=API&period=month&reportUniqueId=CustomDimensions_getCustomDimension_idDimension--1&
+                                       token_auth=",token,"&translateColumnNames=1"),
+                                skipNul = TRUE,encoding = "UTF-8", check.names = FALSE )
   
   
+  names(data_organization)[1] <- "label"
   
-  languages <- x$groups[[1]]$display_name
+  data_organization$label <- as.character(data_organization$label)
   
-  languages_1 <- languages %>% mutate(id = x$id) %>% select(id, de)
+  organizations_zuerich <- data_organization[grep("kanton-zuerich", data_organization$label),"label"]
+  
+  
+  return(organizations_zuerich)
   
 }
 
 
-test_4 <-  map(test_1_list, function(x) extract_language(x))
 
-test_5 <- do.call(rbind.data.frame, test_4)
+
+
+
+organizations <- getOrganizationsZH(token)
+
+
+
+
+
+getOpendataSwissData <- function(organization){
+  
+  ckanr_setup(url = "https://opendata.swiss/")
+  
+  data_all <- package_search(fq = paste0('organization:',organization), rows= 1000, as='table')
+  
+  data_results <- data_all$results
+  
+  
+  data_with_groups <- data_results %>% mutate(groups_de = data_results$groups %>%  map(~getgroups(.) ) %>% as_vector,
+                                              title = data_results$title$de,
+                                              organization_url = data_results$organization$image_display_url,
+                                              organization_name = data_results$organization$name)
+  
+  
+  data_needed <- data_with_groups %>% select(name, title, organization_url, groups_de, organization_name)
+
+  
+  
+}
+
+
+
+getgroups <- function(x){
+  
+  
+  
+  group <- x %>% extract2("display_name")  %>% group_by() %>% summarise_each(funs(paste(., collapse = " / "))) %$% de
+  
+  
+  return(group)
+  
+}
+
+
+
+test <- getOpendataSwissData(organizations[1])
+
+
+
+
+
+
+
+
+# matomo token needed to query the API
+
+# function to retrieve monthly webstatsdata via matomo Api
+get_matomo_data <- function(month,organization,matomo_token=token){
+  data <- read.csv(paste0("https://piwik.opendata.swiss/index.php?module=API&method=CustomDimensions.getCustomDimension&
+                          format=csv&idSite=1&period=month&idDimension=2&
+                          reportUniqueId=CustomDimensions_getCustomDimension_idDimension--2&
+                          segment=dimension1%253D%253D",organization,"&label=&date=",month,"&
+                          filter_limit=false&format_metrics=1&expanded=1&idDimension=2&token_auth=",matomo_token),
+                   skipNul = TRUE,encoding = "UTF-8", check.names = FALSE )
+  
+  names(data)[1]<- "label"
+  
+  data$date <- month
+  
+  return(data)
+}
+# retrieve data for the different data publishers affiliated with the canton of Zurich
+stats_time_data <- get_matomo_data(month,organization=organizations[1], matomo_token =  token)
+
+
+stats_time_data$label <- as.character(stats_time_data$label)
+
+
+test_total <- left_join(test, stats_time_data, by = c("name"="label"))
+
+
+
+
+
+
+
+
+
+
+
 
 
