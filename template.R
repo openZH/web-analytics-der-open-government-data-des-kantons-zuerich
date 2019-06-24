ad21ca30f0f31ef8775a6345e126a4230f216744
Amsler Andreas <B105PAA@ji.ktzh.ch>
2019-06-24 16:31:25 +0200

    Ergänzung Dokumentation

--- template.R
+++ template.R
@@ -10,23 +10,24 @@
 #3. adjust the month in the object name
 #4. adjust the month in the function: month = ....
 #5. adjust the name in the function: name = "..."
-#6. curser on line 19 and press ctrl+Enter to run the function
+#6. curser on line 25 and press ctrl+Enter to run the function
 #7. have a look at the file -> in global environment klick on the object
-#8. adjust the filename in line 27
-#9. curser on line 27 and press ctrl+Enter to export the data as a csv-file
+#8. adjust the filename in line 32
+#9. curser on line 32 and press ctrl+Enter to export the data as a csv-file
 
-source("C:/gitrepos/web-analytics-der-open-government-data-des-kantons-zuerich/data_download.R")
+source("data_download.R")
 
 # matomo token needed to query the API
-matomo_token <- ""
+matomo_token <- "fdf8fc140d2b204596e6668dec590697"
 
 
 # function that gets the data
-OGDanalytics_2019_05 <- getWebAnalytics(
-  month = "2019-05-31",
+OGDanalytics_2019_02 <- getWebAnalytics(
+  month = "2019-02-28",
   matomo_token = matomo_token,
   name = "kanton-zuerich"
 )
 
 # function that exports the data
-writeWebAnalytics(OGDanalytics_2019_05, "L:/STAT/08_DS/06_Diffusion/OGD/Datenproduzenten_ZH/Open-ZH/ZH_Datasets_UniqueActions_2019-05.csv")
+writeWebAnalytics(OGDanalytics_2019_02, "L:/STAT/08_DS/06_Diffusion/OGD/Datenproduzenten_ZH/Open-ZH/ZH_Datasets_UniqueActions_2019-02.csv")
+
